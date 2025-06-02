31d12d4e39f047fe97ba07d1978cc7a6ebae6bc9
Juan Deniro Tambun <juan.deniro@ui.ac.id>
2025-06-02 16:53:54 +0700

    Update routes.py

--- blueprints/library/routes.py
+++ blueprints/library/routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask_login import login_required
 
 from models import Resource
-from blueprints.library import library_bp
+from blueprints import library_bp
 
 @library_bp.route("/")
 @login_required
